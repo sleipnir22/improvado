154c896c3cc608c58da4e6a5840252558da5ba8f
Denis Bolovin <bolovindenis@gmail.com>
2023-03-07 17:44:46 +0700

    conversion to dataframe extracted to 1 function in utils.py

--- improvado/services/csv_report_generator.py
+++ improvado/services/csv_report_generator.py
@@ -1,8 +1,8 @@
 import io
-import pandas as pd
 
 from improvado.dataschemas.request import FriendsGetRequest
 from improvado.services.base import ReportGenerator, Report, ReportType
+from improvado.services.utils import users_to_df
 
 
 class CsvReportGenerator(ReportGenerator):
@@ -10,21 +10,6 @@ class CsvReportGenerator(ReportGenerator):
     def generate_report(self, user_id: int) -> Report:
         request = FriendsGetRequest(user_id=user_id)
         users = self.client.get_user_friends(request)
-        df = pd.DataFrame()
-        for item in users.response.items:
-            df = pd.concat(
-                [df, pd.Series(
-                    {
-                        "id": item.id,
-                        "first_name": item.first_name,
-                        "last_name": item.last_name,
-                        "country": item.country.title if item.country is not None else "not found",
-                        "city": item.city.title if item.city is not None else "not found",
-                        "sex": item.sex.name,
-                        "bdate": item.bdate.replace(".", "-") if item.bdate is not None else "not found"
-                    }
-                ).to_frame().T],
-                ignore_index=True,
-            )
-        file_stream = io.StringIO(df.to_csv(index=False), newline="\n")
+        df = users_to_df(users)
+        file_stream = io.StringIO(df.to_csv(index=False))
         return file_stream
---
+++ improvado/services/utils.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+from improvado.dataschemas.response import FriendsGetResponse
+
+
+def users_to_df(users: FriendsGetResponse):
+    df = pd.DataFrame()
+    for item in users.response.items:
+        df = pd.concat(
+            [df, pd.Series(
+                {
+                    "id": item.id,
+                    "first_name": item.first_name,
+                    "last_name": item.last_name,
+                    "country": item.country.title if item.country is not None else "not found",
+                    "city": item.city.title if item.city is not None else "not found",
+                    "sex": item.sex.name,
+                    "bdate": item.bdate.replace(".", "-") if item.bdate is not None else "not found"
+                }
+            ).to_frame().T],
+            ignore_index=True,
+        )
+    return df
