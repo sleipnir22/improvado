f6d0db500ac4e40ad7aa6ec2aef43732d660c8de
Denis Bolovin <bolovindenis@gmail.com>
2023-03-07 17:45:10 +0700

    TSV report generator

---
+++ improvado/services/tsv_report_generator.py
@@ -0,0 +1,15 @@
+import io
+
+from improvado.dataschemas.request import FriendsGetRequest
+from improvado.services.base import ReportGenerator, Report, ReportType
+from improvado.services.utils import users_to_df
+
+
+class CsvReportGenerator(ReportGenerator):
+    REPORT_TYPE = ReportType.tsv
+    def generate_report(self, user_id: int) -> Report:
+        request = FriendsGetRequest(user_id=user_id)
+        users = self.client.get_user_friends(request)
+        df = users_to_df(users)
+        file_stream = io.StringIO(df.to_csv(index=False, sep = "\t"))
+        return file_stream
